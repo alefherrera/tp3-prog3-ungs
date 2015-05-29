761ca1a26cdde484d4e2ab77a57463207cf83f69
alefherrera <alef.herrera@gmail.com>
2015-05-28 22:18:42 -0300

    primer commit

---
+++ tp3-prog3/src/main/java/tp3_prog3/tp3_prog3/App.java
@@ -0,0 +1,13 @@
+package tp3_prog3.tp3_prog3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
