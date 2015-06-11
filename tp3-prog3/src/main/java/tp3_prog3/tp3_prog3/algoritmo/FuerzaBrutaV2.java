1804650bf671054600ae545384de382b68c4b533
alefherrera <alef.herrera@gmail.com>
2015-06-11 14:11:22 -0300

    mejora de algoritmo

--- tp3-prog3/src/main/java/tp3_prog3/tp3_prog3/algoritmo/FuerzaBrutaV2.java
+++ tp3-prog3/src/main/java/tp3_prog3/tp3_prog3/algoritmo/FuerzaBrutaV2.java
@@ -1,7 +1,5 @@
 package tp3_prog3.tp3_prog3.algoritmo;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,57 +9,56 @@ import tp3_prog3.tp3_prog3.model.Respuesta;
 
 public class FuerzaBrutaV2 {
 
-	
 	public static Respuesta resolver(List<Jugador> universo) {
-		_universo = universo;
-		resultados = new ArrayList<Respuesta>();
-		generarCombinaciones();
-		
-		resultados.sort(new Comparator<Respuesta>() {
-
-			@Override
-			public int compare(Respuesta o1, Respuesta o2) {
-				if (o1.getPuntaje() < o2.getPuntaje())
-					return 1;
-				if (o1.getPuntaje() == o2.getPuntaje())
-					return 0;
-				return -1;
-			}
-		});
-		
-		return resultados.get(0);		
+		Respuesta resultado = new Respuesta();
+		int[] arr = new int[universo.size()];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i;
+		}
+		int[] result = new int[Respuesta.capacidad];
+		return combinaciones(arr, Respuesta.capacidad, 0, result, resultado,
+				universo);
 	}
-	
-	private static List<Jugador> _universo;
-	private static List<Respuesta> resultados;
-	
-	public static void probarPosiciones(int[] posiciones){
-		
+
+	private static Respuesta combinaciones(int[] arr, int len,
+			int startPosition, int[] result, Respuesta resultado,
+			List<Jugador> u) {
+		if (len == 0) {
+			// cada vez que llego al final pruebo todas las combinaciones
+			return probarPosiciones(result, resultado, u);
+		}
+		for (int i = startPosition; i <= arr.length - len; i++) {
+			result[result.length - len] = arr[i];
+			// con un break aca podria generar un mejor backtracking
+			resultado = combinaciones(arr, len - 1, i + 1, result, resultado, u);
+		}
+		return resultado;
+	}
+
+	private static Respuesta probarPosiciones(int[] posiciones,
+			Respuesta resultado, List<Jugador> u) {
+
 		Respuesta r = new Respuesta();
-		//System.out.println("probando: " + Arrays.toString(posiciones));
+
 		for (int i : posiciones) {
-			Jugador j = _universo.get(i);
+			Jugador j = u.get(i);
 			if (cumpleTodo(r, j)) {
 				r.agregar(j);
-			}
-			else
-				return;
+			} else
+				return resultado;
 		}
-		//System.out.println("ENCONTRRRRE UNOOO");
-			resultados.add(r);
-		
-		
-	}
-	
-	
-	
-	private static boolean cumpleTodo(Respuesta r, Jugador j) {
-		return !r.getJugadores().contains(j)
-				&& r.getCantidad() < Respuesta.capacidad
-				&& cumpleFormacion(r, j) && cumpleSeleccion(r, j)
-				&& cumpleAmarillas(r, j) && cumpleRojas(r, j);
+
+		return r.getPuntaje() > resultado.getPuntaje() ? r : resultado;
 	}
 
+	private static boolean cumpleTodo(Respuesta r, Jugador j) {
+		return r.getCantidad() < Respuesta.capacidad && cumpleFormacion(r, j)
+				&& cumpleSeleccion(r, j) && cumpleAmarillas(r, j)
+				&& cumpleRojas(r, j);
+	}
+
+	// posicion 0 = arquero, 1 = defensor, 2 = mediocampista, 3 = delantero //
+	// definido en enum de Posiciones.
 	private static final int[] maximos = { 1, 4, 3, 3 };
 
 	private static boolean cumpleFormacion(Respuesta r, Jugador j) {
@@ -74,11 +71,12 @@ public class FuerzaBrutaV2 {
 		return ret;
 	}
 
+	private static final int MaxSeleccion = 5;
+
 	private static boolean cumpleSeleccion(Respuesta r, Jugador j) {
-		for (Map.Entry<Pais, Integer> entry : r.paises().entrySet())
-		{
-		    if (entry.getValue() > 5)
-		    	return false;
+		for (Map.Entry<Pais, Integer> entry : r.paises().entrySet()) {
+			if (entry.getValue() > MaxSeleccion)
+				return false;
 		}
 
 		return true;
@@ -95,29 +93,5 @@ public class FuerzaBrutaV2 {
 	private static boolean cumpleRojas(Respuesta r, Jugador j) {
 		return r.getRojas() + j.getTarjetasRojas() <= maxRojas;
 	}
-	
-	private static void generarCombinaciones(){
-		int[] arr = new int[_universo.size()];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i;
-		}
-		int[] result = new int[Respuesta.capacidad];
-		combinaciones(arr, Respuesta.capacidad, 0, result);
-		return;
-	}
-	
-	private static void combinaciones(int[] arr, int len, int startPosition, int[] result){
-        if (len == 0){
-            //System.out.println(Arrays.toString(result));
-        	probarPosiciones(result);
-        	
-        	
-            return;
-        }       
-        for (int i = startPosition; i <= arr.length-len; i++){
-            result[result.length - len] = arr[i];
-            combinaciones(arr, len-1, i+1, result);
-        }
-    }   
-	
+
 }
